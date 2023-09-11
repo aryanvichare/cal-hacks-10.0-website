b34514e03a3e3a4b1e7029d72a4e408d67591fd7
aryanvichare <aryanvichare10@gmail.com>
2023-09-10 23:30:45 -0700

    Update volunteer sign up form

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,7 +45,7 @@ export default function Home() {
           ,{" "}
           <a
             target='_blank'
-            href='https://x.calhacks.io/volunteer-interest'
+            href='x.calhacks.io/volunteers-signup-10-0'
             className='underline'>
             volunteer
           </a>
